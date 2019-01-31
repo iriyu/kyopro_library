1544aaa717ec9191ab8ba20cdafe1af9d7181d7a
Iori Yugo <yugo@IorinoMacBook-puro.local>
2019-01-31 18:08:49 +0900

    add UnionFind

--- src/algorithm/UnionFind.h
+++ src/algorithm/UnionFind.h
@@ -1,1 +1,23 @@
-//test
+struct UnionFind {
+    vector<int> p, r;
+    int gn;
+    UnionFind(int n = 0) : p(n, -1), r(n, 1), gn(n) {}
+    void merge(int a, int b) {
+        int x = group(a), y = group(b);
+        if (x == y) return;  // same
+        gn--;
+        if (r[x] < r[y])
+            p[x] = y;
+        else if (r[x] > r[y])
+            p[y] = x;
+        else {
+            p[x] = y;
+            r[x]++;
+        }
+    }
+    int group(int a) {
+        if (p[a] == -1) return a;
+        return p[a] = group(p[a]);
+    }
+    bool same(int a, int b) { return group(a) == group(b); }
+};
